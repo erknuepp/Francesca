4c8ed79c37827427f002754c0a3f22c62373d139
Edward Knueppel <edward.knueppel@zywave.com>
2022-02-03 16:14:43 -0600

    Get token

--- Assignment4/planespotters.py
+++ Assignment4/planespotters.py
@@ -11,25 +11,26 @@ def main():
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         result = requests.get(url, headers=headers)
         
-
+        cookies = result.cookies
+        print(cookies)
         # Look at the unmodified source.
-        print(result.content + '\n')
+        #print(result.content + '\n')
 
         # # Here we extract the token required to login
         soupy = BeautifulSoup(result.content, 'html.parser')
         token = soupy.find(id="csrf")['value'];
 
-        print(token)
+        #print(token)
 
         # # Always pause between two requests.
 
         time.sleep(5) # 5s
 
         # # An open session carries the cookies and allows you to make post requests
-        # session_requests = requests.session()
+        #session_requests = requests.session()
 
         # res = session_requests.post(URL,
-        #                         data = {"referer" : "http://flightaware.com/",
+        #                         data = {"referer" : "https://www.planespotters.net",
         #                               "mode" : "login",
         #                               "flightaware_username" : "", # your username here
         #                               "flightaware_password" : "", # your password here
