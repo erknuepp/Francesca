67d651a4e7d8d759510a7b90cba486730f0f438d
Edward Knueppel <edward.knueppel@zywave.com>
2022-02-03 21:20:12 -0600

    Part 1 Spec 2 Post

--- Assignment4/planespotters.py
+++ Assignment4/planespotters.py
@@ -5,12 +5,12 @@ import time
 
 def main():
     try:
-        url = 'https://www.planespotters.net/user/login'
+        URL = 'https://www.planespotters.net/user/login'
 
+        HEADERS = {"referer":"https://www.planespotters.net/",
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        result = requests.get(URL, headers=HEADERS)
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        result = requests.get(url, headers=headers)
-        
         cookies = result.cookies
         print(cookies)
         # Look at the unmodified source.
@@ -18,46 +18,44 @@ def main():
 
         # # Here we extract the token required to login
         soupy = BeautifulSoup(result.content, 'html.parser')
-        token = soupy.find(id="csrf")['value'];
-
-        #print(token)
+        csrf = soupy.find(id="csrf")['value']
+        
+        print(csrf)
+        
 
         # # Always pause between two requests.
 
-        time.sleep(5) # 5s
+        time.sleep(5)  # 5s
 
         # # An open session carries the cookies and allows you to make post requests
-        #session_requests = requests.session()
+        session_requests = requests.session()
 
-        # res = session_requests.post(URL,
-        #                         data = {"referer" : "https://www.planespotters.net",
-        #                               "mode" : "login",
-        #                               "flightaware_username" : "", # your username here
-        #                               "flightaware_password" : "", # your password here
-        #                               "token" : token},
-        #                         headers = dict(referer = "http://flightaware.com/"),
-        #                         timeout = 15)
+        res = session_requests.post(URL,
+                                    data={"referer": "https://www.planespotters.net/",
+                                          "username": "edward.knueppel.jr@gmail.com",  # your username here
+                                          "password": "Cat4sale__--",  # your password here
+                                          "csrf": csrf},
+                                    headers=HEADERS,
+                                    timeout=15)
         # #
         # # This will get us the Cookies.
         # #
-        # cookies = session_requests.cookies.get_dict()
+        cookies = session_requests.cookies.get_dict()
 
         # #
         # # And this is the easiest way to remain in session.
         # #
-        # page2 = session_requests.get(URL,
-        #                               cookies=cookies)
+        page2 = session_requests.get(URL, cookies=cookies, headers=HEADERS)
 
-        # doc2 = BeautifulSoup(page2.content, 'html.parser')
+        doc2 = BeautifulSoup(page2.content, 'html.parser')
 
-        # print(doc2);
-        # print();
-        # print(cookies)
-        # print(bool(doc2.findAll(text = ""))) # your username here
-        # print(bool(doc2.findAll(text = " My FlightAware")))
+        print(doc2)
+        print(cookies)
+        print(bool(doc2.findAll(text="© Planespotters.net 2022. All rights reserved.")))
 
     except Exception as ex:
         print('error: ' + str(ex))
+
 
 if __name__ == '__main__':
     main()
